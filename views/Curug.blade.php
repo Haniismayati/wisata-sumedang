88062c04876873d679f1be42503046fe2d3455c6
Hani ismayati <93498577+Haniismayati@users.noreply.github.com>
2022-01-10 08:22:41 +0700

    Add files via upload

---
+++ views/Curug.blade.php
@@ -0,0 +1,45 @@
+@extends('master')
+@section('content')
+<div style="background-color:#CFD6CC;">
+<div class="container">
+<div align='center'>
+  <div class="col-sm">
+    <br>
+    <br>
+    <div class="card" style="width: 40rem;">
+      <h3 class="card-title">CURUG CIGOROBOG</h3>
+      <img src="https://sikidang.com/wp-content/uploads/Curug-Cigorobog.jpg" class="card-img-top" alt="gambar laptop">
+       <div class="card-body">
+         <p class="card-text">Curug cigorobog ini cocok banget buat kamu berlibur ditengah sibuk dan capenya kegiatanmu, karena suasana curug yang dingin dan sejuk dapat membuatmu kembali menjadi fresh.
+         <br>
+         <br>
+         Sekarang, Aku mau kasih tau alamat curug cigorobog.
+         <br>
+        Curug Cigorobog berada di Citengah, Kec. Sumedang Sel., Kabupaten Sumedang, Jawa Barat. Destinasi wisata Curug Gorobog ini berjarak kurang lebih 10 km dari pusat kota Sumedang.
+        <br>
+        <br>
+        Untuk tiket masuk ke curug cigorobog ini kamu cuma mengeluarkan uang Rp 5000/orang. Sedangkan parkir motor Rp2000, dan Rp5000 jika membawa mobil. Namun, semua dapat berubah sewaktu waktu.</p>
+        <br>Gimana? siap berlibur ke Curug Cigorobog? ajak teman, sahabat dan keluargamu segera.
+       </div>
+    </div>
+  </div>
+
+  <div class="col-sm">
+    <div class="card" style="width: 40rem;">
+      <h3 class="card-title">CURUG SABUK</h3>
+      <img src="https://sumedangtandang.com/upload/foto_blog/cipongkor.jpg" class="card-img-top" alt="gambar laptop">
+       <div class="card-body">
+         <p class="card-text">Curug Sabuk atau Curug Cilengkrang adalah sebuah air terjun yang terletak di Lereng Gunung Ciremai, Lembah Cilengkrang, Jalaksana, Kabupaten Kuningan, provinsi Jawa Barat, Indonesia. Tinggi air terjun ini adalah sekitar 20 m, dengan aliran air cukup deras.</P>
+         <br>Margamekar, Kec. Sumedang Sel., Kabupaten Sumedang, Jawa Barat 45311
+         <br>Curug Sabuk berada di dalam kawasan hutan Gunung Kareumbi, atau tepatnya di sebelah timur laut kawasan Gunung Kareumbi, atau di sekitar kawasan Taman Buru Gunung Masigit. Secara geografis, air terjun ini berlokasi di Dusun Nangorak, Desa Margamekar, Kecamatan Sumedang Selatan. Kabupaten Sumedang. Curug ini cukup populer di kalangan penggemar wisata alam karena posisinya yang jauh dari keramaian penduduk
+         <br>
+         <br>Sebagai tempat wisata, Curug Sabuk memang bisa dikatakan masih sepi atau jarang dikunjungi. Akses yang harus melewati jalanan berliku, serta sedikitnya informasi, menjadi beberapa faktor yang membuat air terjun ini masih belum begitu banyak dijamah. Namun, untuk keelokan dan keindahan, air terjun ini boleh diadu dengan objek wisata sejenis.
+         </div>
+    </div>
+  </div>
+<br>
+<br>
+</div>
+</div>
+</div>
+@endsection
---
+++ views/layout/navbar.blade.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-secondary">
+  <div class="container-fluid">
+  <img src="https://img.icons8.com/material/24/000000/google-maps.png"/>
+    <span class="navbar-brand" href="#">WISATA</span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+
+      <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/beranda">BERANDA</a>
+      </li>
+
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/home">BERSEJARAH</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/Curug">CURUG</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/taman">TAMAN</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/profil">MATA AIR</a>
+        </li>
+
+      </ul>
+    </div>
+
+  </div>
+</nav>
